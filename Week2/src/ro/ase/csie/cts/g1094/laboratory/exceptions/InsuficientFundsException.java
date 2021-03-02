56b3cad0bf175ff2e523a2c2b643e31c7454eb29
ErosTeodorescu <ErosTeodorescu@mihaipopescu>
2021-03-02 14:39:22 +0200

    Week2 Update

---
+++ Week2/src/ro/ase/csie/cts/g1094/laboratory/exceptions/InsuficientFundsException.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1094.laboratory.exceptions;
+
+public class InsuficientFundsException extends Exception {
+	public  InsuficientFundsException(String message) {
+		super(message);
+		
+	}
+	
+	public InsuficientFundsException() {
+		
+	}
+}
